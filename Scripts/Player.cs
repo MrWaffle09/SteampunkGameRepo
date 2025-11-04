acefe80f8cf1a14a602a1f1f30a37d9b4ab77b0e
Splooble <155033535+MrWaffle09@users.noreply.github.com>
2025-11-04 12:24:25 -0500

    player abstract class
    made calss

---
+++ Scripts/Player.cs
@@ -0,0 +1,12 @@
+﻿using Godot;
+
+namespace MultiplayerProject.Scripts;
+
+public abstract class Player
+{
+    
+
+    [Export] public virtual int speed { get; set; } = 100;
+    [Export] public virtual int health { get; set; } = 100;
+
+}
