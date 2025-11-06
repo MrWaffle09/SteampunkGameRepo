b0f045e7374e5039b9e008c83f5b2a6c273a1074
JonJonesCodesForFun <jpgorske08@gmail.com>
2025-11-06 12:07:10 -0500

    Added Animations to all Players

--- Scenes/Player.cs
+++ Scenes/Player.cs
@@ -3,19 +3,19 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
-    [Export] public int Speed { get; set; } = 400;
-    
-    
-    //Handles Movement
-    public void GetInput()
-    {
-        Vector2 inputDirection = Input.GetVector("MenuLeft", "MenuRight", "MenuUp", "MenuDown");
-        Velocity = inputDirection * Speed;
-    }
+	[Export] public int Speed { get; set; } = 400;
+	
+	
+	//Handles Movement
+	public void GetInput()
+	{
+		Vector2 inputDirection = Input.GetVector("MenuLeft", "MenuRight", "MenuUp", "MenuDown");
+		Velocity = inputDirection * Speed;
+	}
 
-    public override void _PhysicsProcess(double delta)
-    {
-        GetInput();
-        MoveAndSlide();
-    }
+	public override void _PhysicsProcess(double delta)
+	{
+		GetInput();
+		MoveAndSlide();
+	}
 }
