9f4c5ea4da931ac9eb19e97f756ef202fdf02bd4
JonJonesCodesForFun <jpgorske08@gmail.com>
2025-11-11 12:17:57 -0500

    Update TileMap
    Updated Player Movement and fixed Hitboxes on TileMap and Player

--- Scenes/Player.cs
+++ Scenes/Player.cs
@@ -3,10 +3,16 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
-	[Export] public int Speed { get; set; } = 400;
-	
-	
-	//Handles Movement
+	[Export] public int Speed { get; set; } = 70;
+
+	private AnimatedSprite2D _anim;
+
+	public override void _Ready()
+	{
+		_anim = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+	}
+
+	// Handles Movement
 	public void GetInput()
 	{
 		Vector2 inputDirection = Input.GetVector("MenuLeft", "MenuRight", "MenuUp", "MenuDown");
@@ -16,6 +22,31 @@ public partial class Player : CharacterBody2D
 	public override void _PhysicsProcess(double delta)
 	{
 		GetInput();
+
+		// ✅ FIXED: Flip sprite the correct direction
+		if (Velocity.X != 0)
+		{
+			// If moving right -> flip OR if moving left -> no flip, depending on your sprite
+			_anim.FlipH = Velocity.X > 0;
+		}
+
+		// Play/stop animations based on movement
+		if (Velocity.LengthSquared() > 0.01f)
+		{
+			if (_anim.Animation != "walk")
+				_anim.Play("walk");
+		}
+		else
+		{
+			if (_anim.SpriteFrames.HasAnimation("default"))
+				_anim.Play("default");
+			else
+			{
+				_anim.Stop();
+				_anim.Frame = 0;
+			}
+		}
+
 		MoveAndSlide();
 	}
 }
