3180d3277dcc474336b7ae436e62a91feafbb9e7
JonJonesCodesForFun <jpgorske08@gmail.com>
2025-11-17 12:17:18 -0500

    Done

--- Scripts/Player1.cs
+++ Scripts/Player1.cs
@@ -5,41 +5,41 @@ using MultiplayerProject.Scripts;
 
 public partial class Player1 : Player
 {
-    public int ID { get; set; } = 0;
-    public int speed { get; set; } = 100;
+	public int ID { get; set; } = 0;
+	public int speed { get; set; } = 100;
 
-    public void Die()
-    {
-        QueueFree();
-    }
+	public override void Die()
+	{
+		QueueFree();
+	}
 
-    
-    public override void _PhysicsProcess(double delta)
-    {
-        //handles movement
-        float leftX = Input.GetJoyAxis(ID, JoyAxis.LeftX);
-        if (Mathf.Abs(leftX) < 0.2)
-        {
-            leftX = 0;
-        }
-        float leftY = Input.GetJoyAxis(ID, JoyAxis.LeftY);
-        if (Mathf.Abs(leftY) < 0.2)
-        {
-            leftY = 0;
-        }
-        Vector2 direction = new Vector2(leftX, leftY);
-        Vector2 velocity = Velocity;
-        if (direction != Vector2.Zero)
-        {
-            velocity = direction * speed;
-        }
-        else
-        {
-            velocity.X = Mathf.MoveToward(velocity.X, 0, speed);
-            velocity.Y = Mathf.MoveToward(velocity.Y, 0, speed);
-        }
+	
+	public override void _PhysicsProcess(double delta)
+	{
+		//handles movement
+		float leftX = Input.GetJoyAxis(ID, JoyAxis.LeftX);
+		if (Mathf.Abs(leftX) < 0.2)
+		{
+			leftX = 0;
+		}
+		float leftY = Input.GetJoyAxis(ID, JoyAxis.LeftY);
+		if (Mathf.Abs(leftY) < 0.2)
+		{
+			leftY = 0;
+		}
+		Vector2 direction = new Vector2(leftX, leftY);
+		Vector2 velocity = Velocity;
+		if (direction != Vector2.Zero)
+		{
+			velocity = direction * speed;
+		}
+		else
+		{
+			velocity.X = Mathf.MoveToward(velocity.X, 0, speed);
+			velocity.Y = Mathf.MoveToward(velocity.Y, 0, speed);
+		}
 
-        Velocity = velocity;
-        MoveAndSlide();
-    }
+		Velocity = velocity;
+		MoveAndSlide();
+	}
 }
