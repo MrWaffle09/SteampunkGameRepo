bfbe690b9713c56523cc81bde82610be1e34d5ac
Splooble <155033535+MrWaffle09@users.noreply.github.com>
2025-11-20 12:33:06 -0500

    Started Charcteer Selector

---
+++ Scripts/GameManager.cs
@@ -0,0 +1,13 @@
+using Godot;
+using System;
+
+public static class GameManager
+{
+    public static int CapyID = 0;
+    public static int MechID = 0;  
+    public static int MicroID = 0;
+    public static int BullID = 0;
+    
+
+    
+}
