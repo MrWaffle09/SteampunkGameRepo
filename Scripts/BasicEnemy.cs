53224385cedf282d25970d92db4929714f420f04
Isaiah-hub-org <ipierre0@ftstudent.org>
2025-11-24 11:25:46 -0500

    enemy

--- Scripts/BasicEnemy.cs
+++ Scripts/BasicEnemy.cs
@@ -4,26 +4,31 @@ using System;
 public partial class BasicEnemy: CharacterBody2D
 {
     
-    public int Speed = 200; 
+    public int Speed = 20; 
     private CharacterBody2D Player;
 
     
 
     public void OnPlayerEntered(Node2D node) {
         if (node is CharacterBody2D){
-            GD.Print("Body entered"); 
+             
             Vector2 direction = (node.GlobalPosition - this.GlobalPosition).Normalized();
 
             Velocity = direction * Speed;
-
         
         }
     }
-    public void PhysicsProcess(float delta) {
+    public override void _PhysicsProcess(double delta) {
+        
 
         MoveAndSlide();
     }
-
+      
+    
+    public void OnPlayerOut(Node2D node){
+      Velocity = Vector2.Zero;
+        
+    }
 
 }
 
